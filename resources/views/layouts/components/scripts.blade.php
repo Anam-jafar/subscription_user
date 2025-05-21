56c59bf6bbe72c81408085ca25123aae7e89314f
Anam-jafar <anamibnjafar@gmail.com>
2025-05-21 11:26:42 +0800

    CLEAN UP : Removed unused js files

--- resources/views/layouts/components/scripts.blade.php
+++ resources/views/layouts/components/scripts.blade.php
@@ -1,6 +1,6 @@
 <!-- SCROLL-TO-TOP -->
 <div class="scrollToTop">
-    <span class="arrow"><i class="ti ti-arrow-narrow-up text-xl"></i></span>
+  <span class="arrow"><i class="ti ti-arrow-narrow-up text-xl"></i></span>
 </div>
 <div id="responsive-overlay"></div>
 
@@ -9,22 +9,6 @@
 
 <!-- NODE WAVES JS -->
 <script src="{{ asset('subscription/build/assets/libs/node-waves/waves.min.js') }}"></script>
-
-<!-- COLOR PICKER JS -->
-<script src="{{ asset('subscription/build/assets/libs/@simonwep/pickr/pickr.es5.min.js') }}"></script>
-<script src="{{ asset('subscription/build/assets/libs/flatpickr/flatpickr.min.js') }}"></script>
-
-<!-- SWITCH JS -->
-<script src="{{ asset('subscription/build/assets/switch.js') }}"></script>
-
-<!-- PRELINE JS -->
-<script src="{{ asset('subscription/build/assets/libs/preline/preline.js') }}"></script>
-
-<!-- SIMPLEBAR JS -->
-<script src="{{ asset('subscription/build/assets/libs/simplebar/simplebar.min.js') }}"></script>
-@vite(['resources/assets/js/simplebar.js'], 'subscription/build')
-
-
 
 <!-- Auto Complete JS -->
 <script src="{{ asset('subscription/build/assets/libs/@tarekraafat/autocomplete.js/autoComplete.min.js') }}"></script>
