287f6fa0f621f11324d8712aeb62447128b205f0
Anam-jafar <anamibnjafar@gmail.com>
2025-05-21 17:59:11 +0800

    CLEAN UP : route clean up

--- routes/web.php
+++ routes/web.php
@@ -8,80 +8,35 @@ use App\Http\Controllers\BaseController;
 use Illuminate\Support\Facades\Storage;
 use Illuminate\Http\Request;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "web" middleware group. Make something great!
-|
-*/
-
-
-
-
 Route::prefix('subscription')->group(function () {
-    Route::get('/login', [AuthController::class, 'showLoginForm'])->name('subscriptionLogin');
-    Route::match(['get', 'post'], '/login-email', [AuthController::class, 'showLoginByEmail'])->name('subscriptionLoginEmail');
-    Route::get('/login-phone', [AuthController::class, 'showLoginByMobile'])->name('subscriptionLoginPhone');
-    Route::match(['get', 'post'], '/login-otp/{email}', [AuthController::class, 'fillOtpLogin'])->name('subscriptionLoginOtp');
-    Route::get('/search-bandar', [BaseController::class, 'getBandar'])->name('search.bandar');
 
-
-
-    Route::middleware(['customAuth'])->group(function () {
-        Route::get('/activate-subscription/{id}', [BaseController::class, 'activateSubscription'])
-            ->name('activateSubscription');
-
-        Route::get('/activated-subscription/{id}', [BaseController::class, 'activatedSubscription'])
-            ->name('activatedSubscription');
-
-        Route::match(['get', 'post'], '/pending-subscription/{id}', [BaseController::class, 'pendingSubscription'])
-            ->name('pendingSubscription');
-
-        Route::get('/home', [BaseController::class, 'home'])->name('home');
-
-        Route::get('/statement/list', [FinancialStatementController::class, 'list'])->name('statementList');
-        Route::match(['get', 'post'], '/statement/create/{id}', [FinancialStatementController::class, 'create'])->name('createStatement');
-        Route::match(['get', 'post'], '/statement/edit/{id}', [FinancialStatementController::class, 'edit'])->name('editStatement');
-        Route::match(['get', 'post'], '/statement/view/{id}', [FinancialStatementController::class, 'view'])->name('viewStatement');
-        Route::post('/statement/edit-request/{id}', [FinancialStatementController::class, 'editRequest'])->name('editRequestStatement');
-
-        Route::get('/request-subscription/{id}', [BaseController::class, 'requestSubscription'])->name('requestSubscription');
-        Route::match(['get', 'post'], '/institute/edit', [InstituteController::class, 'edit'])->name('instituteEdit');
+    /*
+    |--------------------------------------------------------------------------
+    | Routes Without Auth
+    |--------------------------------------------------------------------------
+    */
+    Route::controller(BaseController::class)->group(function () {
+        Route::get('/find-institute', 'findInstitute')->name('findInstitute');
+        Route::get('/search-institutes', 'searchInstitutes')->name('searchInstitutes');
+        Route::get('/get-cities', 'getCities')->name('getCities');
+        Route::get('/search-bandar', 'getBandar')->name('search.bandar');
+        Route::get('/get-institutes', 'getInstitutesByCity')->name('getInstitutes');
+        Route::post('/institute-check', 'instituteCheck')->name('instituteCheck');
+        Route::match(['get', 'post'], '/institute-details/{id}', 'instituteDetails')->name('instituteDetails');
     });
 
-    // Routes that don't require authentication
-    Route::get('/payment-link/{id}/{c_id}', [BaseController::class, 'makePayment'])->name('makePayment');
-    Route::post('/logout', [AuthController::class, 'logout'])->name('subscriptionLogout');
+    Route::controller(AuthController::class)->group(function () {
+        Route::get('/login', 'showLoginForm')->name('subscriptionLogin');
+        Route::match(['get', 'post'], '/login-email', 'showLoginByEmail')->name('subscriptionLoginEmail');
+        Route::get('/login-phone', 'showLoginByMobile')->name('subscriptionLoginPhone');
+        Route::match(['get', 'post'], '/login-otp/{email}', 'fillOtpLogin')->name('subscriptionLoginOtp');
+        Route::match(['get', 'post'], '/fill-otp/{email}', 'fillOtp')->name('fillOtp');
+        Route::post('/logout', 'logout')->name('subscriptionLogout');
+    });
 
-
-
-
-
-
-
-
-    Route::get('/find-institute', [BaseController::class, 'findInstitute'])->name('findInstitute');
     Route::match(['get', 'post'], '/institute-registration/{id}', [InstituteController::class, 'instituteRegistration'])->name('instituteRegistration');
 
-
-
-
-    Route::get('/search-institutes', [BaseController::class, 'searchInstitutes'])->name('searchInstitutes');
-    Route::get('/get-cities', [BaseController::class, 'getCities'])->name('getCities');
-    Route::get('/get-institutes', [BaseController::class, 'getInstitutesByCity'])->name('getInstitutes');
-    Route::post('/institute-check', [BaseController::class, 'instituteCheck'])->name('instituteCheck');
-
-
-    Route::match(['get', 'post'], '/institute-details/{id}', [BaseController::class, 'instituteDetails'])->name('instituteDetails');
-    Route::match(['get', 'post'], '/fill-otp/{email}', [AuthController::class, 'fillOtp'])->name('fillOtp');
-
-
-
-
+    Route::get('/payment-link/{id}/{c_id}', [BaseController::class, 'makePayment'])->name('makePayment');
 
     Route::get('/download/attachment/{year}/{filename}', function ($year, $filename) {
         $path = "/var/www/static_files/fin_statement_attachments/$year/$filename";
@@ -92,6 +47,33 @@ Route::prefix('subscription')->group(function () {
 
         abort(404);
     })->name('download.attachment');
+
+
+
+    /*
+    |--------------------------------------------------------------------------
+    | Routes with Auth
+    |--------------------------------------------------------------------------
+    */
+
+    Route::middleware(['customAuth'])->group(function () {
+
+        Route::controller(BaseController::class)->group(function () {
+            Route::get('/home', 'home')->name('home');
+            Route::get('/request-subscription/{id}', 'requestSubscription')->name('requestSubscription');
+        });
+
+        Route::controller(FinancialStatementController::class)->group(function () {
+            Route::get('/statement/list', 'list')->name('statementList');
+            Route::match(['get', 'post'], '/statement/create/{id}', 'create')->name('createStatement');
+            Route::match(['get', 'post'], '/statement/edit/{id}', 'edit')->name('editStatement');
+            Route::match(['get', 'post'], '/statement/view/{id}', 'view')->name('viewStatement');
+            Route::post('/statement/edit-request/{id}', 'editRequest')->name('editRequestStatement');
+        });
+
+        Route::match(['get', 'post'], '/institute/edit', [InstituteController::class, 'edit'])->name('instituteEdit');
+    });
+
 
 
 });
